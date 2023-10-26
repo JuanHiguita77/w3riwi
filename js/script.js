6a26c630151eb0650bfbc8acc99397156b29ba22
YEPES-HIGUITA <juanferhiguita65@gmail.com>
2023-10-25 21:17:22 -0500

    Proyecto terminado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-bars = document.querySelector('.bars')
+bars = document.querySelector('.bars');
 bars.onclick = function()
 {
-    navbar = document.querySelector('#menu')
-    navbar.classList.toggle('active')
+    navbar = document.querySelector('#menu');
+    navbar.classList.toggle('active');
 }
+
+textarea = document.querySelector('#textarea');
+
+result = document.querySelector('#resultarea');
+
+textarea.addEventListener('input', () =>
+{
+    const htmlCode = textarea.value;
+
+    result.srcdoc = htmlCode;
+});
